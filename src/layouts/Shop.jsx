91c2c5b19cc0ce32654335aecd6962d7f14422b2
rastogi-archi <rastogiarchi18@gmail.com>
2024-03-31 14:45:01 +0530

    exclusive section completed

--- src/layouts/Shop.jsx
+++ src/layouts/Shop.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Popular from '../components/Popular'
+import Exclusive from '../components/Exclusive'
 
 const Shop = () => {
     return (
         <div>
             <Hero />
             <Popular />
+            <Exclusive />
         </div>
     )
 }
