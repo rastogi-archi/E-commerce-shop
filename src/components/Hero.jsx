94970e0bc3728c375ad0cace93ec923ab69ffd53
rastogi-archi <rastogiarchi18@gmail.com>
2024-03-31 21:04:16 +0530

    changes in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     return (
         <main className='h-80 bg-gradient-to-b from-pink-200 to-white mt-2 font-[Roboto] flex items-center justify-center'>
             <div className='ml-2 sm:ml-32 md:ml-36 xl:ml-80'>
-                <p className='uppercase text-sm font-medium'>new arrivals only</p>
+                <p className='uppercase text-xs font-medium mb-1 sm:mb-0 sm:text-sm'>new arrivals only</p>
                 <div className='flex items-center  mt-[-20px] sm:mt-[-30px]'>
                     <p className=' text-3xl sm:text-4xl font-bold md:text-5xl'>new</p>
                     <img src="\hand.png" alt="" className='h-20 md:h-32' />
