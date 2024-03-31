ace5051d613318f3e1f11a97abc73dcfe1bfa0d8
rastogi-archi <rastogiarchi18@gmail.com>
2024-03-31 18:39:12 +0530

    change in item component

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Item = (props) => {
     return (
         <div className='font-[Roboto] sm:w-52 text-left ml-2 mr-2 mt-5'>
-            <img src={props.image} alt="" className='h-24 sm:h-32 md:h-52 m-auto' />
-            <p className='text-[6px] font-bold sm:text-xs text-gray-600 text-center' >{props.name}</p>
+            <img src={props.image} alt="" className='h-24 sm:h-32 md:h-52 m-auto hover:scale-105' />
+            <p className='text-[6px] font-bold sm:text-xs text-gray-600 text-center mt-2' >{props.name}</p>
             <div className='flex gap-2 mt-1 justify-start ml-2'>
                 <div className='text-[8px] sm:text-sm'>
                     ${props.new_price}
